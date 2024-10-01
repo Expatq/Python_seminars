4813d7384c60ffb673f44c5d915e528dd009e572
Mosevnin Kirill <116762207+Expatq@users.noreply.github.com>
2024-10-01 17:37:55 +0300

    Add files via upload

---
+++ ZombieOOP.py
@@ -0,0 +1,20 @@
+class Monster:
+    def __init__(self, name:str, destruct:int):
+        self.name = name
+        self.power = destruct
+        print(f"The monster {self.name} the {self.__class__.__name__} was created")
+
+    def growl(self, s:str):
+        print(f"{s} growled {self.name} the {self.__class__.__name__}")
+
+    def attack(self):
+        print(f"The monster {self.name} the {self.__class__.__name__} attacked with force {self.power}")
+
+
+class Zombie(Monster):
+    def __init__(self, name:str, destruct=5):
+        super().__init__(name, destruct)
+        
+    def growl(self, up = False):
+        s = "Raaaauuughhhh" if not up else "Raaaauuughhhh".upper()
+        super().growl(s)
